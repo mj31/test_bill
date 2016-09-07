f8b6706e5f2ad4a7f58170c995100773f0472d4f
gaocy15 <gaocy15@ziroom.com>
2016-09-07 19:17:22 +0800

    init

--- src/main/java/com/mj/bill/controller/UserController.java
+++ src/main/java/com/mj/bill/controller/UserController.java
@@ -11,16 +11,15 @@ import com.mj.bill.pojo.User;
 import com.mj.bill.service.IUserService;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 	@Resource
 	private IUserService userService;
 	
-	@RequestMapping("/showUser")
+	@RequestMapping("/login")
 	public String toIndex(HttpServletRequest request,Model model){
-		int userId = Integer.parseInt(request.getParameter("id"));
-		User user = this.userService.getUserById(userId);
+		//int userId = Integer.parseInt(request.getParameter("id"));
+		User user = this.userService.getUserById(1);
 		model.addAttribute("user", user);
-		return "showUser";
+		return "login/login";
 	}
 }
